b0864c15a79e2b068ceed5b11528844c6151d577
LuuneTheMoon <titoulelio@live.fr>
2024-04-22 21:03:56 +0200

    Basic CRUD Reservation

---
+++ GestionHotel.Apis/Services/IReservationService.cs
@@ -0,0 +1,13 @@
+﻿using GestionHotel.Apis.Models;
+
+namespace GestionHotel.Apis.Services
+{
+    private interface IReservationService
+    {
+        Task<bool> AddReservation(Reservation reservation);
+        Task<Room> GetReseravtionById(int id);
+        Task<bool> GetReservationByDates(DateTime start_date, DateTime end_date);
+        Task<Room> UpdateReservation(Reservation reservation);
+        Task<bool> DeleteReservation(int id);
+    }
+}
