f4f596aaf1a85bbeb73eff451b02ed70ec6265e5
Hugo Enguehard <hugo.enguehard3468@hotmail.com>
2024-04-22 19:40:19 +0200

    Basic CRUD Structure for Room Model

---
+++ GestionHotel.Apis/Services/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using GestionHotel.Apis.Models;
+
+namespace GestionHotel.Apis.Services
+{
+    public interface IRoomService
+    {
+        Task<bool> AddRoom(Room room);
+        Task<Room> GetRoomById(int id);
+        Task<bool> GetRoomByDates(DateTime start_date, DateTime end_date);
+        Task<Room> UpdateRoom(Room room);
+        Task<bool> DeleteRoom(int id);
+    }
+}
---
+++ GestionHotel.Apis/Services/RoomService.cs
@@ -0,0 +1,49 @@
+﻿using GestionHotel.Apis.Models;
+
+namespace GestionHotel.Apis.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly ApiContext _context;
+
+        public RoomService(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddRoom(Room room)
+        {
+            try
+            {
+                _context.Rooms.Add(room);
+                await _context.SaveChangesAsync();
+                return true;
+            } 
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+
+        public Task<bool> DeleteRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetRoomByDates(DateTime start_date, DateTime end_date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Room> GetRoomById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Room> UpdateRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
